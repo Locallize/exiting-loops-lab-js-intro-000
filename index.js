5ec1cc33a3887102c868f319b60fdf991915a194
beneficial-kernel-0493 <fillanlee@aol.com>
2019-01-15 03:19:13 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,35 @@
+let breakOut = (array, changeValue, stopValue) =>{
+  for (let i = 0; i < array.length ;i++){
+    if(array[i] === stopValue){
+      break;
+    }
+    array[i] = changeValue;
+  }
+  return array;
+};
+
+let keepGoing = (array, changeValue, skipValue)=>{
+  for (let i = 0; i <array.length; i++){
+    if(array[i] === skipValue){
+      continue;
+    }
+    array[i] = changeValue;
+  }
+  return array;
+};
+let findBy = (array, findFn)=>{
+  for (let i = 0; i < array.length; i++){
+    if(findFn){
+      return findFn;
+    }
+  }
+  return null;
+};
+// let findBy = (array, findFn)=>{
+//   for (let i = 0; i < array.length; i++){
+//     if(array[i] !== findFn){
+//       return null
+//     }
+//     return findFn;
+//   }
+// };
